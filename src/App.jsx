5faf293cf21652d4eb2f5c5eb29a702bf36d1a03
samarth goyal <samarthagrawal1804@gmail.com>
2023-09-02 23:21:07 +0530

    Added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-    <h1 className='text-center text-2xl'>Hello Developers</h1>
-    </>
+   <div className='w-full h-screen bg-black flex justify-center items-center'>
+    <div className=' bg-gray-700 max-w-md w-full px-4 py-2 rounded-md'>
+      <div className=''>
+        <h1 className='text-center text-3xl text-white mt-3 mb-6'>Password Generator</h1>
+        <input type="text" className='px-2 py-1' />
+        <button className='bg-blue-600 text-white px-2 py-1'>Copy</button>
+      </div>
+    </div>
+   </div>
   )
 }
 
